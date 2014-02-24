3bff643cbb98b70b9fb2d63a2b04041e6e2d66e3
J. Tanner Netterville <tannern@gmail.com>
2014-02-24 11:24:58 -0600

    clean up shader

--- public/javascript/shaders/VelvetyShader.js
+++ public/javascript/shaders/VelvetyShader.js
@@ -13,12 +13,11 @@ define([
     'text!shaders/VelvetyShader-fragment.glsl'
 ], function (THREE, vertexShaderCode, fragmentShaderCode) {
     THREE.VelvetyShader = {
-        //side: THREE.DoubleSide,
+        side: THREE.DoubleSide,
         vertexColors: THREE.VertexColors,
         transparent: true,
         uniforms: {
-            'overrideVertexColor': {type: 'i', value: false},
-            'newVertexColor': {type: 'v3', value: new THREE.Vector3(1.0,1.0,1.0)},
+            'opacity': {type: 'f', value: 1.0},
             'ambientFactor': {type: 'f', value: 0.3},
             'directFactor': {type: 'f', value: 1.0}
         },
