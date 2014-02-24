8758329eac9fef5b5d0a5da2b0c4633134258cce
ghemingway <graham.hemingway@gmail.com>
2014-02-24 12:12:53 -0600

    Bit of cleanup and initial gulp build support.

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+var gulp = require('gulp'),
+    uglify = require('gulp-uglify'),
+    rjs = require('gulp-requirejs');
+
+gulp.task('default', function() {
+    rjs({
+        baseUrl: 'public/javascript',
+        out: 'build.js',
+        paths: {
+            jquery:             'libs/jquery.min',
+            jstree:             'libs/jstree.min',
+            THREE:              'libs/three.min',
+            underscore:         'libs/underscore-min',
+            TrackballControls:  'libs/TrackballControls',
+            dat:                'libs/dat.gui.min',
+            VIS:                'libs/visualize'
+        },
+        shim: {
+            jquery: {
+                exports: "$"
+            },
+            jstree: {
+                deps: ["jquery"]
+            },
+            underscore: {
+                exports: "_"
+            },
+            THREE: {
+                exports: "THREE"
+            },
+            TrackballControls: {
+                deps: ["THREE"]
+            },
+            dat: {
+                exports: "dat"
+            },
+            VIS: {
+                exports: "VIS",
+                deps: ["jquery"]
+            }
+        },
+        name: 'main'
+    })
+    .pipe(uglify())
+    .pipe(gulp.dest('./public/javascript/')); // pipe it to the output DIR
+});
