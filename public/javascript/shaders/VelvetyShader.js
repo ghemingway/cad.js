64260f78e2ddf3e6dfb2ad0afeef4feccaea799a
J. Tanner Netterville <tannern@gmail.com>
2014-02-27 18:42:04 -0600

    make VelvetyShader single sided

--- public/javascript/shaders/VelvetyShader.js
+++ public/javascript/shaders/VelvetyShader.js
@@ -14,7 +14,7 @@ define([
 ], function (THREE, vertexShaderCode, fragmentShaderCode) {
     THREE.VelvetyShader = function () {
         return {
-            side: THREE.DoubleSide,
+            //side: THREE.DoubleSide,
             vertexColors: THREE.VertexColors,
             transparent: true,
             uniforms: {
